b372e3a45f94f08bbb004e2d0608aa1425a26532
Andrey Popov <andrey_popov@me.com>
2018-11-25 17:41:09 +0300

    fixed fatal error

--- wirenboard.js
+++ wirenboard.js
@@ -43,7 +43,6 @@ module.exports = function(RED) {
      *
      */
     RED.httpAdmin.get(NODE_PATH + 'itemlist', function (req, res) {
-
         var config = req.query;
         var controller = RED.nodes.getNode(config.controllerID);
         var forceRefresh = config.forceRefresh ? ['1', 'yes', 'true'].includes(config.forceRefresh.toLowerCase()) : false;
@@ -51,13 +50,16 @@ module.exports = function(RED) {
         if (controller && controller instanceof WirenBoardServerNode) {
             controller.getItemsList(function (items) {
                 if (items) {
-                    res.json(items).end();
+                    res.json(items);
+                } else {
+                    res.status(404).end();
                 }
             }, forceRefresh);
         } else {
-            // res.status(404).end();
+            res.status(404).end();
         }
     });
+
 
 
 
@@ -287,13 +289,13 @@ module.exports = function(RED) {
 
         // Sort of singleton construct
         if (forceRefresh || node.items === undefined) {
-            console.log('Refreshing devices');
+            node.log('Refreshing devices');
             var that = this;
             that.devices = [];
             that.items = [];
+            that.end = false;
 
-            var client  = mqtt.connect('mqtt://'+node.url)
-
+            var client  = mqtt.connect('mqtt://'+node.url);
             client.on('connect', function () {
                 client.subscribe(['/devices/+/meta/name', '/devices/+/controls/+/meta/+', '/devices/+/controls/+', '/tmp/items_list'], function (err) {
                     if (!err) {
@@ -302,69 +304,73 @@ module.exports = function(RED) {
                         RED.log.error("wirenboard: error code #0023: "+err);
                     }
                 })
-            })
+            });
 
             client.on('error', function (error) {
-                RED.log.error("wirenboard: error code #0024: "+error);
-            })
+                 RED.log.error("wirenboard: error code #0024: "+error);
+            });
 
             client.on('message', function (topic, message) {
                 if (message.toString() == 'end_reading_items_list') {
-                    client.unsubscribe(['/devices/+/meta/name', '/devices/+/controls/+/meta/+', '/devices/+/controls/+', '/tmp/items_list'], function (err) {})
-                    client.end()
+                    //client.unsubscribe(['/devices/+/meta/name', '/devices/+/controls/+/meta/+', '/devices/+/controls/+', '/tmp/items_list'], function (err) {})
+                    client.end(true);
 
                     if (!that.items.length) {
                         RED.log.warn("wirenboard: error code #0026: No items, check your settings");
+                    } else {
+                        that.items = (that.items).sort(function (a, b) {
+                            var aSize = a.device_name;
+                            var bSize = b.device_name;
+                            var aLow = a.control_name;
+                            var bLow = b.control_name;
+                            if (aSize == bSize) {
+                                return (aLow < bLow) ? -1 : (aLow > bLow) ? 1 : 0;
+                            } else {
+                                return (aSize < bSize) ? -1 : 1;
+                            }
+                        })
                     }
-// console.log(that.devices)
-                    callback((that.items).sort(function (a, b) {
-                        var aSize = a.device_name;
-                        var bSize = b.device_name;
-                        var aLow = a.control_name;
-                        var bLow = b.control_name;
-                        if (aSize == bSize) {
-                            return (aLow < bLow) ? -1 : (aLow > bLow) ? 1 : 0;
-                        } else {
-                            return (aSize < bSize) ? -1 : 1;
-                        }
-                    }))
+
+                    if (!that.end) {
+                        that.end = true;
+                        callback(that.items);
+                    }
+                    return node.items;
+                } else {
+                    //parse topic
+                    var topicParts = topic.split('/');
+                    var deviceName = topicParts[2];
+
+                    //meta device name
+                    if (topicParts[3] == 'meta' && topicParts[4] == 'name') {
+                        that.devices[deviceName] = {'friendly_name': message.toString(), 'controls': []}
+
+                        //meta controls
+                    } else if (topicParts[3] == 'controls' && topicParts[5] == 'meta') {
+                        var controlName = topicParts[4]
+                        if (typeof(that.devices[deviceName]['controls'][controlName]) == 'undefined')
+                            that.devices[deviceName]['controls'][controlName] = {}
+
+                        that.devices[deviceName]['controls'][controlName][topicParts[6]] = message.toString()
+
+                        //devices
+                    } else if (topicParts[3] == 'controls') {
+                        var controlName = topicParts[4]
+                        that.items.push({
+                            topic: topic,
+                            message: message.toString(),
+                            control_name: controlName,
+                            device_name: deviceName,
+                            device_friendly_name: typeof(that.devices[deviceName]['friendly_name']) != 'undefined' ? that.devices[deviceName]['friendly_name'] : deviceName,
+                            meta: that.devices[deviceName]['controls'][controlName]
+                        });
+                    }
                 }
-
-                //parse topic
-                var topicParts = topic.split('/')
-                var deviceName = topicParts[2]
-
-                //meta device name
-                if (topicParts[3] == 'meta' && topicParts[4] == 'name') {
-                    that.devices[deviceName] = {'friendly_name':message.toString(), 'controls':[]}
-
-                //meta controls
-                } else if (topicParts[3] == 'controls' && topicParts[5] == 'meta') {
-                    var controlName = topicParts[4]
-                    if (typeof(that.devices[deviceName]['controls'][controlName]) == 'undefined')
-                        that.devices[deviceName]['controls'][controlName] = {}
-
-                    that.devices[deviceName]['controls'][controlName][topicParts[6]] = message.toString()
-
-                //devices
-                } else if (topicParts[3] == 'controls') {
-                    var controlName = topicParts[4]
-                    that.items.push({
-                        topic:topic,
-                        message:message.toString(),
-                        control_name:controlName,
-                        device_name:deviceName,
-                        device_friendly_name:typeof(that.devices[deviceName]['friendly_name'])!='undefined'?that.devices[deviceName]['friendly_name']:deviceName,
-                        meta:that.devices[deviceName]['controls'][controlName]
-                    });
-                }
-
-
-
             })
         } else {
-            console.log('Using cached devices');
+            node.log('Using cached devices');
             callback(node.items);
+            return node.items;
         }
 
     }
