02b7e9e5d86e36589ebd7cb99e77ddebe3278720
Andrey Popov <andrey_popov@me.com>
2021-02-28 18:12:27 +0300

    Motor node timer, position by percent

--- nodes/motor.js
+++ nodes/motor.js
@@ -12,36 +12,49 @@ module.exports = function(RED) {
             node.runningTimer = null;
             node.runningInterval = null;
             node.is_subscribed = false;
-            node.percent = null;
+            node.percent = 0; //close by default
+            node.contact_open_flag = null;
+            node.contact_close_flag = null;
+            //homekit: 0 close, 100 open
 
             //get server node
             node.server = RED.nodes.getNode(node.config.server);
 
             if (node.server) {
 
+                node.listener_onMQTTConnect = function(data) { node.onMQTTConnect(); }
+                node.server.on('onMQTTConnect', node.listener_onMQTTConnect);
+                node.listener_onConnectError = function(data) { node.onConnectError(); }
+                node.server.on('onConnectError', node.listener_onConnectError);
+                node.listener_onMQTTMessage = function(data) { node.onMQTTMessage(data); }
+                node.server.on('onMQTTMessage', node.listener_onMQTTMessage);
+                node.on('close', () => this.onMQTTClose());
+                if (typeof(node.server.mqtt) === 'object') {
+                    node.onMQTTConnect();
+                }
+
                 node.on('input', function (message_in) {
                     clearTimeout(node.cleanTimer);
                     clearTimeout(node.runningTimer);
+                    clearInterval(node.runningInterval);
 
-                    if (parseInt(message_in.payload) > 1 && parseInt(message_in.payload) <= 100) {
-                        //choose direction
-                        var dir = parseInt(message_in.payload) > 0;
-                        dir = node.config.inverse ? !dir : dir;
-                        dir = dir ? "1" : "0";
-
-                        node.go(message_in.payload, dir);
-                        // node.start(dir);
-                    } else if ("stop" === message_in.payload) {
-                        node.stop();
-                    } else if ("toggle" === message_in.payload) {
-                        node.toggle();
+                    //homekit format
+                    if (typeof(message_in.payload) == "object") {
+                        if ("TargetPosition" in message_in.payload) {
+                            node.go(message_in.payload.TargetPosition);
+                        }
                     } else {
-                        //choose direction
-                        var dir = parseInt(message_in.payload) > 0;
-                        dir = node.config.inverse ? !dir : dir;
-                        dir = dir ? "1" : "0";
-
-                        node.start(dir);
+                        if ("open" === message_in.payload) {
+                            node.go(100);
+                        } else if ("close" === message_in.payload) {
+                            node.go(0);
+                        } else if ("stop" === message_in.payload) {
+                            node.stop();
+                        } else if ("toggle" === message_in.payload) {
+                            node.go(node.percent > 0?0:100);
+                        } else if (parseInt(message_in.payload) >= 0 && parseInt(message_in.payload) <= 100) {
+                            node.go(message_in.payload);
+                        }
                     }
                 });
 
@@ -55,110 +68,190 @@ module.exports = function(RED) {
 
         }
 
-        go(percent, dir) {
+        go(percent) {
             var node = this;
             percent = parseInt(percent);
-
-            var timeEnd = node.config.max_running_time/100*percent;
-            if (percent === 0) {
+            var absPercent = Math.abs(node.percent-percent);
+            var timeEnd = node.config.max_running_time/100*absPercent;
+            if (percent === 100 || percent === 0) { //open
                 timeEnd = node.config.max_running_time;
             }
 
+            if (node.config.contact_close && node.config.contact_close in node.server.devices_values) {
+                if (parseInt(node.server.devices_values[node.config.contact_close])) { //is closed
+                    node.percent = 0;
+                }
+            } else if (node.config.contact_open && node.config.contact_open in node.server.devices_values) {
+                if (parseInt(node.server.devices_values[node.config.contact_open])) { //is opened
+                    node.percent = 100;
+                }
+            }
 
+            console.log("From: "+node.percent+" to "+percent + '  -> '+timeEnd+'ms');
 
-            node.server.mqtt.publish(node.config.channel_dir + '/on', dir.toString());
-            node.log('Published to mqtt topic: ' + node.config.channel_dir + '/on : ' + dir.toString());
+            node.move(percent>node.percent);
 
-            //enable
-            node.server.mqtt.publish(node.config.channel + '/on', "1");
-            node.log('Published to mqtt topic: ' + node.config.channel + '/on : 1');
             node.send({
                 payload: {
                     on: true,
-                    dir: dir,
+                    target: percent,
+                    position: node.percent,
                     inverse: node.config.inverse,
                     elementId:WirenboardHelper.generateElementId(node.config.channel)
                 },
-                topic: node.config.channel
+                topic: node.config.channel,
+                homekit: {
+                    CurrentPosition: node.percent,
+                    PositionState: percent>node.percent?0:1,
+                    TargetPosition: percent
+                }
             });
 
-            node.runningInterval = setTimeout(function () {
-                //node.percent +=
-            }, 500);
+            node.runningInterval = setInterval(function () {
+                if (percent > node.percent) {
+                    node.percent++;
+                    if (node.percent > 100) node.percent = 100;
+                } else if (percent < node.percent) {
+                    node.percent--;
+                    if (node.percent < 0) node.percent = 0;
+                }
 
+                if (percent !== node.percent) {
+                    node.nodeStatus((percent > node.percent ? "opening" : "closing") + ": " + node.percent + "%");
+                }
+            }, node.config.max_running_time/100);
 
             //disable
             node.runningTimer = setTimeout(function () {
-                clearInterval(node.runningInterval);
-                node.stop();
                 node.percent = percent;
-            }, timeEnd);
-        }
-
-        start(dir) {
-            var node = this;
-
-            node.server.mqtt.publish(node.config.channel_dir + '/on', dir.toString());
-            node.log('Published to mqtt topic: ' + node.config.channel_dir + '/on : ' + dir.toString());
-
-            //enable
-            node.server.mqtt.publish(node.config.channel + '/on', "1");
-            node.log('Published to mqtt topic: ' + node.config.channel + '/on : 1');
-            node.send({
-                payload: {
-                    on: true,
-                    dir: dir,
-                    inverse: node.config.inverse,
-                    elementId:WirenboardHelper.generateElementId(node.config.channel)
-                },
-                topic: node.config.channel
-            });
-
-            //disable
-            node.runningTimer = setTimeout(function () {
                 node.stop();
-            }, node.config.max_running_time);
+            }, timeEnd);
         }
 
         stop() {
             var node = this;
 
+            clearTimeout(node.runningTimer);
+            clearInterval(node.runningInterval);
+
             node.server.mqtt.publish(node.config.channel + '/on', "0");
             node.log('Published to mqtt topic: ' + node.config.channel + '/on : 0');
+
             node.send({
                 payload: {
                     on: false,
                     dir: null,
+                    position: node.percent,
                     inverse: node.config.inverse,
                     elementId:WirenboardHelper.generateElementId(node.config.channel)
                 },
-                topic: node.config.channel
+                topic: node.config.channel,
+                homekit: {
+                    CurrentPosition: node.percent,
+                    PositionState: 2,
+                    TargetPosition: node.percent
+                }
             });
 
             node.nodeStatus("node-red-contrib-wirenboard/motor:status.stopped");
         }
 
-        toggle() {
+        nodeStatus(text) {
+            var node = this;
+            node.status({fill: "green",shape: "dot",text: text});
+            clearTimeout(node.cleanTimer);
+            node.cleanTimer = setTimeout(function () {
+                node.status({fill: "grey",shape: "ring",text: node.percent+"%"});
+            }, 3000);
+        }
+
+        onMQTTMessage(data) {
             var node = this;
 
-            if (typeof (node.config.channel_dir) == 'string' && (node.config.channel_dir).length) {
-                if (node.config.channel_dir in node.server.devices_values) {
-                    var lastDir = node.server.devices_values[node.config.channel_dir];
-                    var newDir = lastDir.toString()==="1"?"0":"1";
-                    node.start(newDir);
-                    node.nodeStatus("node-red-contrib-wirenboard/motor:status.toggle");
+            //функция доводчика: усилие мотора намного сильнее на старте
+            //как только датчик контакта замкнулся, останавливаем мотор, выжидаем пару секунд и дожимаем окно
+            if (node.config.contact_close && data.topic === node.config.contact_close) {
+                var isClose = parseInt(data.payload);
+
+                //fix incorrect status
+                if (isClose === 0 && node.percent === 0) { //sensor open BUT percent in close
+                    node.percent = 100;
+                }
+
+                if (isClose) {
+                    node.percent = 0;
+                    node.stop();
+                }
+
+                if (node.contact_close_flag !== isClose) {
+                    if (isClose && node.contact_close_flag != null) {
+                        if (node.config.extra_close_push_delay && node.config.extra_close_push_time) {
+                            setTimeout(function () {
+                                node.close();
+
+                                setTimeout(function () {
+                                    node.percent = 0;
+                                    node.stop();
+                                }, node.config.extra_close_push_time);
+
+                            }, node.config.extra_close_push_delay);
+                        }
+                    }
+                    node.contact_close_flag = isClose;
                 }
             }
         }
 
-        nodeStatus(text) {
-            var node = this;
-            node.status({fill: "green",shape: "dot",text: text});
-            node.cleanTimer = setTimeout(function () {
-                node.status({});
-            }, 3000);
+        open() {
+            this.move(true);
         }
 
+        close() {
+            this.move(false);
+        }
+
+        move(direction) {
+            var node = this;
+
+            if (node.config.inverse) direction = !direction;
+            direction = direction?"1":"0";
+
+            node.server.mqtt.publish(node.config.channel_dir + '/on', direction);
+            node.log('Published to mqtt topic: ' + node.config.channel_dir + '/on : ' + direction);
+
+            //enable
+            node.server.mqtt.publish(node.config.channel + '/on', "1");
+            node.log('Published to mqtt topic: ' + node.config.channel + '/on : 1');
+        }
+
+        onConnectError(status = null) {
+            var node = this;
+            node.status({
+                fill: "red",
+                shape: "dot",
+                text: "node-red-contrib-wirenboard/motor:status.no_connection"
+            });
+        }
+
+        onMQTTClose() {
+            var node = this;
+
+            //remove listeners
+            if (node.listener_onMQTTConnect) {
+                node.server.removeListener('onMQTTConnect', node.listener_onMQTTConnect);
+            }
+            if (node.listener_onConnectError) {
+                node.server.removeListener('onConnectError', node.listener_onConnectError);
+            }
+            if (node.listener_onMQTTMessage) {
+                node.server.removeListener("onMQTTMessage", node.listener_onMQTTMessage);
+            }
+
+            node.onConnectError();
+        }
+
+        onMQTTConnect() {
+        }
 
     }
     RED.nodes.registerType('wirenboard-motor', WirenboardNodeMotor);
